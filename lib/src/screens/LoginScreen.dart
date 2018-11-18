e0ed7363269f23761b32f029c74fc26fafef4a79
Dragos <dragos.giugula@gmail.com>
2018-11-18 11:50:29 +0100

    implementing PW and Email Fields

--- lib/src/screens/LoginScreen.dart
+++ lib/src/screens/LoginScreen.dart
@@ -7,11 +7,34 @@ class LoginScreen extends StatelessWidget{
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          
+          emailField(),
+          passwordField(),
         ],
       )
     );
 
     
   }
+Widget emailField() {
+      return TextField(
+         keyboardType: TextInputType.emailAddress,
+         decoration: InputDecoration(
+           hintText: "You@Example.com",
+           labelText: "Email Address",
+         ), 
+
+      );
+}
+
+Widget passwordField() {
+  return TextField(
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: "Password",
+      labelText: "Password",
+    ),
+
+  );
+}
+  
 }
