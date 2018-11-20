3d0ee53f465734821e5b84fd4535c08eabdce704
Dragos <dragos.giugula@gmail.com>
2018-11-20 14:26:07 +0100

    extending Object for bloc with validators

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,8 +1,19 @@
 import "dart:async";
+import 'validators.dart';
 
-class Bloc {
-  final _emailController = StreamController<String>();
-  final _passwordComtroller = StreamController<String>();
 
+
+class Bloc extends Object with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+// Getter that allow us to add data to stream 
+Stream<String> get email => _email.stream;
+Stream<String> get passwrod => _password.stream;
+
+// Second Bloc of Getters - which will allow us to change data
+
+Function(String) get changeEmail => _email.sink.add;
+Function(String) get changePassword => _password.sink.add;
 
 }
