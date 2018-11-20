dcaab3fd94a85e36faa361259dbe7d264dcb412e
Dragos <dragos.giugula@gmail.com>
2018-11-20 14:29:29 +0100

    adding dispose to clear controllers

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -8,12 +8,16 @@ class Bloc extends Object with Validators {
   final _password = StreamController<String>();
 
 // Getter that allow us to add data to stream 
-Stream<String> get email => _email.stream;
-Stream<String> get passwrod => _password.stream;
+Stream<String> get email => _email.stream.transform(validateEmail);
+Stream<String> get passwrod => _password.stream.transform(validatePassword);
 
 // Second Bloc of Getters - which will allow us to change data
 
 Function(String) get changeEmail => _email.sink.add;
 Function(String) get changePassword => _password.sink.add;
 
+dispose() {
+  _email.close();
+  _password.close();
+}
 }
