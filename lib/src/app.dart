314792e04654866aa8b678d285640b642cab4372
Dragos <dragos.giugula@gmail.com>
2018-11-21 13:21:06 +0100

    warpping material app in provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/LoginScreen.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   build(contenxt) {
-    return MaterialApp(
+    return Provider(
+      child: MaterialApp(
       title: 'Log Me In',
       home: Scaffold(
         appBar: AppBar(title: Text('New APp'),),
         body: LoginScreen(),
-      )
+        )
 
+      ),
     );
+    
   }
 }
